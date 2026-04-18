e0c0bf8d06f5e120beb66d53e82aa1dce886646d
Stephan Duijkers <mail@sduijkers.com>
2026-04-18 12:00:23 +0200

    Added more function info to doc

--- 3581427607/Documentation.lua
+++ 3581427607/Documentation.lua
@@ -50,5 +50,126 @@ show_message = show_message;
 ---With this function you can create a new instance of the specified object at any given point within the room and at the depth specified.
 ---@type fun(x: number, y: number, depth: number, obj: any): any
 instance_create_depth = instance_create_depth;
+---With this function you can check if a key is pressed, as imput it uses virtual key codes
+---@type fun(virtualKey: number)
+keyboard_check_pressed = keyboard_check_pressed;
+--------------------------------------------
+--------------------------------------------
+-- Virtual Key Codes
+-- 
+-- // Control keys
+-- Backspace = 8,
+-- Tab = 9,
+-- Enter = 13,
+-- Shift = 16,
+-- Ctrl = 17,
+-- Alt = 18,
+-- PauseBreak = 19,
+-- CapsLock = 20,
+-- Escape = 27,
+-- Space = 32,
+-- PageUp = 33,
+-- PageDown = 34,
+-- End = 35,
+-- Home = 36,
+-- ArrowLeft = 37,
+-- ArrowUp = 38,
+-- ArrowRight = 39,
+-- ArrowDown = 40,
+-- Insert = 45,
+-- Delete = 46,
+-- 
+-- // Number row
+-- D0 = 48,
+-- D1 = 49,
+-- D2 = 50,
+-- D3 = 51,
+-- D4 = 52,
+-- D5 = 53,
+-- D6 = 54,
+-- D7 = 55,
+-- D8 = 56,
+-- D9 = 57,
+-- 
+-- // Letters
+-- A = 65,
+-- B = 66,
+-- C = 67,
+-- D = 68,
+-- E = 69,
+-- F = 70,
+-- G = 71,
+-- H = 72,
+-- I = 73,
+-- J = 74,
+-- K = 75,
+-- L = 76,
+-- M = 77,
+-- N = 78,
+-- O = 79,
+-- P = 80,
+-- Q = 81,
+-- R = 82,
+-- S = 83,
+-- T = 84,
+-- U = 85,
+-- V = 86,
+-- W = 87,
+-- X = 88,
+-- Y = 89,
+-- Z = 90,
+-- 
+-- // Windows / Meta
+-- LeftWindows = 91,
+-- RightWindows = 92,
+-- Select = 93,
+-- 
+-- // Numpad
+-- NumPad0 = 96,
+-- NumPad1 = 97,
+-- NumPad2 = 98,
+-- NumPad3 = 99,
+-- NumPad4 = 100,
+-- NumPad5 = 101,
+-- NumPad6 = 102,
+-- NumPad7 = 103,
+-- NumPad8 = 104,
+-- NumPad9 = 105,
+-- Multiply = 106,
+-- Add = 107,
+-- Subtract = 109,
+-- Decimal = 110,
+-- Divide = 111,
+-- 
+-- // Function keys
+-- F1 = 112,
+-- F2 = 113,
+-- F3 = 114,
+-- F4 = 115,
+-- F5 = 116,
+-- F6 = 117,
+-- F7 = 118,
+-- F8 = 119,
+-- F9 = 120,
+-- F10 = 121,
+-- F11 = 122,
+-- F12 = 123,
+-- 
+-- // Lock keys
+-- NumLock = 144,
+-- ScrollLock = 145,
+-- 
+-- // Punctuation
+-- Semicolon = 186,
+-- Equal = 187,
+-- Comma = 188,
+-- Minus = 189,
+-- Period = 190,
+-- Slash = 191,
+-- Backquote = 192,
+-- BracketLeft = 219,
+-- Backslash = 220,
+-- BracketRight = 221,
+-- Quote = 222
 --------------------------------------------
 --------------------------------------------
