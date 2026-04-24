4ac9406247e528ca48173847560cd4c6144eac06
Stephan Duijkers <mail@sduijkers.com>
2026-04-24 18:12:41 +0200

    Refactor

--- ModFramework Example Mod/ModFramework/ModFrameworkEngineering.lua
+++ ModFramework Example Mod/ModFramework/ModFrameworkEngineering.lua
@@ -29,11 +29,11 @@ function Engineering.AddMech(mech_number, name)
 	local arraySize = #list_mech
 	local itemIndex = arraySize + 1
 	local addedMech = Private.AddMechItemInstance()
-	addedMech.my_num = 			arraySize
-	addedMech.mech_number = 	mech_number
-	addedMech.new_module = 		false
+	addedMech.my_num = arraySize
+	addedMech.mech_number = mech_number
+	addedMech.new_module = true
 	if(name ~= nil) then
-		addedMech.mech_name = 	name
+		addedMech.mech_name = name
 	end
 	list_mech[itemIndex] = addedMech
 
@@ -51,14 +51,14 @@ function Engineering.AddWeapon(weapon_number, huge)
 	--Copy the array to the working set
 	local list_weapon = obj_content_weapons.list_weapon
 
-	local array_size = #list_weapon
-	local itemIndex = array_size + 1
-	local added_weapon = Private.AddWeaponItemInstance()
-	added_weapon.my_num = 			array_size
-	added_weapon.weapon_number = 	weapon_number
-	added_weapon.size_huge = 		huge
-	added_weapon.new_module = 		true
-	list_weapon[itemIndex] = added_weapon
+	local arraySize = #list_weapon
+	local itemIndex = arraySize + 1
+	local addedWeapon = Private.AddWeaponItemInstance()
+	addedWeapon.my_num = arraySize
+	addedWeapon.weapon_number = weapon_number
+	addedWeapon.size_huge = huge
+	addedWeapon.new_module = true
+	list_weapon[itemIndex] = addedWeapon
 
 	--return new data
 	obj_content_weapons.list_weapon = list_weapon
@@ -73,18 +73,17 @@ function Engineering.AddSolenoid(solenoid_number)
 	--Copy the array to the working set
 	local list_solenoid = obj_content_solenoid.list_solenoid
 
-	local number_of_items = obj_content_solenoid.number_of_items + 1
-	local obj_solenoid_item = asset_get_index("obj_solenoid_item")
-	list_solenoid[number_of_items] = instance_create_depth(0, 0, 0, obj_solenoid_item)
-	local added_solenoid = list_solenoid[number_of_items]
-	added_solenoid.my_num = 			number_of_items - 1
-	added_solenoid.solenoid_number = 	solenoid_number
-	added_solenoid.start_x = 			1506
-	added_solenoid.start_y = 			266
+	local arraySize = #list_solenoid
+	local itemIndex = arraySize + 1
+	local addedSolenoid = Private.AddSolenoidItemInstance()
+	addedSolenoid.my_num = arraySize
+	addedSolenoid.solenoid_number = solenoid_number
+	addedSolenoid.new_module = true
+	list_solenoid[itemIndex] = addedSolenoid
 
 	--return new data
 	obj_content_solenoid.list_solenoid = list_solenoid
-	obj_content_solenoid.number_of_items = number_of_items
+	obj_content_solenoid.number_of_items = #list_solenoid
 end
 
 ---Adds a component of type reactor to engineering
@@ -95,18 +94,17 @@ function Engineering.AddReactor(reactor_number)
 	--Copy the array to the working set
 	local list_reactor = obj_content_reactor.list_reactor
 
-	local number_of_items = obj_content_reactor.number_of_items + 1
-	local obj_reactor_item = asset_get_index("obj_reactor_item")
-	list_reactor[number_of_items] = instance_create_depth(0, 0, 0, obj_reactor_item)
-	local added_reactor = list_reactor[number_of_items]
-	added_reactor.my_num = 			number_of_items - 1
-	added_reactor.reactor_number = 	reactor_number
-	added_reactor.start_x = 		8
-	added_reactor.start_y = 		266
+	local arraySize = #list_reactor
+	local itemIndex = arraySize + 1
+	local addedReactor = Private.AddReatorItemInstance()
+	addedReactor.my_num = arraySize
+	addedReactor.reactor_number = reactor_number
+	addedReactor.new_module = true
+	list_reactor[itemIndex] = addedReactor
 
 	--return new data
 	obj_content_reactor.list_reactor = list_reactor
-	obj_content_reactor.number_of_items = number_of_items
+	obj_content_reactor.number_of_items = #list_reactor
 end
 
 ---Adds a component of type weapon to engineering
@@ -117,18 +115,17 @@ function Engineering.AddMagnet(magnet_number)
 	--Copy the array to the working set
 	local list_magnet = obj_content_magnet.list_magnet
 
-	local number_of_items = obj_content_magnet.number_of_items + 1
-	local obj_magnet_item = asset_get_index("obj_magnet_item")
-	list_magnet[number_of_items] = instance_create_depth(0, 0, 0, obj_magnet_item)
-	local added_magnet = list_magnet[number_of_items]
-	added_magnet.my_num = 			number_of_items - 1
-	added_magnet.magnet_number = 	magnet_number
-	added_magnet.start_x = 			8
-	added_magnet.start_y = 			266
+	local arraySize = #list_magnet
+	local itemIndex = arraySize + 1
+	local addedMagnet = Private.AddMagnetItemInstance()
+	addedMagnet.my_num = arraySize
+	addedMagnet.magnet_number = magnet_number
+	addedMagnet.new_module = true
+	list_magnet[itemIndex] = addedMagnet
 
 	--return new data
 	obj_content_magnet.list_magnet = list_magnet
-	obj_content_magnet.number_of_items = number_of_items
+	obj_content_magnet.number_of_items = #list_magnet
 end
 
 ---Create a new obj_mech_item instance
@@ -139,10 +136,31 @@ function Private.AddMechItemInstance()
 end
 
 ---Create a new obj_weapon_item instance
----@return game_obj_weapon_item objMechItem the new obj_weapon_item instance
+---@return game_obj_weapon_item objWeaponItem the new obj_weapon_item instance
 function Private.AddWeaponItemInstance()
 	local obj_weapon_item = asset_get_index("obj_weapon_item")
 	return instance_create_depth(0, 0, 0, obj_weapon_item)
+end
+
+---Create a new obj_solenoid_item instance
+---@return game_obj_solenoid_item objSolenoidItem the new obj_solenoid_item instance
+function Private.AddSolenoidItemInstance()
+	local obj_solenoid_item = asset_get_index("obj_solenoid_item")
+	return instance_create_depth(0, 0, 0, obj_solenoid_item)
+end
+
+---Create a new obj_reactor_item instance
+---@return game_obj_reactor_item objReactorItem the new obj_reactor_item instance
+function Private.AddReatorItemInstance()
+	local obj_reactor_item = asset_get_index("obj_reactor_item")
+	return instance_create_depth(0, 0, 0, obj_reactor_item)
+end
+
+---Create a new obj_magnet_item instance
+---@return game_obj_magnet_item objReactorItem the new obj_magnet_item instance
+function Private.AddMagnetItemInstance()
+	local obj_magnet_item = asset_get_index("obj_magnet_item")
+	return instance_create_depth(0, 0, 0, obj_magnet_item)
 end
 
 ------------------------------------------------------------------------------
